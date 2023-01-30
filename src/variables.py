b4ad82649a8d25dffb6789968127d9a1be86e61f
Tyler Zimmer <tyler.zimmer@chevron.com>
2023-01-30 16:57:03 -0600

    added stuff

---
+++ src/variables.py
@@ -0,0 +1,5 @@
+## Twitter credentials
+consumer_key = "------------"
+consumer_secret = "------------"
+access_token = "----------"
+access_token_secret = "-----------"
--- twitter_credentials.py
+++ twitter_credentials.py
@@ -11,11 +11,3 @@ ACCESS_TOKEN = '1322171562183872513-1eWeqqfCWknLHRPw9PzrlmYhijOTG6'
 ACCESS_TOKEN_SECRET = 'wlWtrJigKT9hQlV7qyXUJYb0SB7QvlbOd8DuxlnM2gR7W'
 CONSUMER_KEY = 'vwMlW6Pi94eP9dTUIVibLRAuE'
 CONSUMER_SECRET =  'txn4xGjcbWPTWArAmWcpHLtDq8EXOLzDyA3TkCD7G32N1gXRBf'
-
-
-
-
-## Turns out I created the v2 OAuth with .Bearer Token instead of ...
-
-#consumer_key = 'vwMlW6Pi94eP9dTUIVibLRAuE'
-#consumer_secret =  'txn4xGjcbWPTWArAmWcpHLtDq8EXOLzDyA3TkCD7G32N1gXRBf'
