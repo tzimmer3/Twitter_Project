b6308fff284d463ac83081fd1d2a44da675358e7
Tyler Zimmer <tyler.zimmer@chevron.com>
2023-02-02 11:57:18 -0600

    moved cleaning functions to TweetAnalyzer

--- src/tweepyclean.py
+++ src/tweepyclean.py
@@ -1,7 +1,7 @@
 import re
+import string
 import numpy as np
 import pandas as pd
-from textblob import TextBlob
 
 
 class TweetAnalyzer():
@@ -11,16 +11,6 @@ class TweetAnalyzer():
 
     def clean_tweet(self, tweet):
         return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
-
-    def analyze_sentiment(self, tweet):
-        analysis = TextBlob(self.clean_tweet(tweet))
-
-        if analysis.sentiment.polarity > 0:
-            return 1
-        elif analysis.sentiment.polarity == 0:
-            return 0
-        else:
-            return -1
 
     def tweets_to_data_frame(self, tweets):
         df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['tweets'])
@@ -33,3 +23,24 @@ class TweetAnalyzer():
         df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
 
         return df
+
+    ## FUNCTIONS
+    def clean_special_chars(tweet):
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+    def extract_hashtags(tweet):
+        regex = "#(\w+)"
+        hashtag_list = re.findall(regex, tweet)
+        return hashtag_list
+
+    def extract_mentions(tweet):
+        regex = "@(\w+)"
+        mentions_list = re.findall(regex, tweet)
+        return mentions_list
+
+    def count_caption_words(tweet):
+        return sum([i.strip(string.punctuation).isalpha() for i in tweet.split()])
+
+    def remove_stopwords(tweet, stopwords):
+        tweet = [word for word in tweet if word not in stopwords]
+        return tweet
